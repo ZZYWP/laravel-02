928d19c64113c2b3c7250e6603b2b9d86a678501
Your Name <497498746@qq.com>
2018-06-14 15:21:03 +0800

    完成静态页面的编写

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','帮助')
+@section('content')
+<h1>帮助页哦</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','home')
+@section('content')
+<h1>主页哦</h1>
+@stop
