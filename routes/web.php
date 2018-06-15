65f5c87c1c0f792e8236897ef7e4b85a14befcc1
Zeng ZI <497498746@qq.com>
2018-06-15 08:59:04 +0000

    完成用户注册

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::get('/about','StaticPagesController@about')->name('about');
 
 //登录注册
 Route::get('/signup','UsersController@create')->name('signup');
+Route::resource('/users','UsersController');
